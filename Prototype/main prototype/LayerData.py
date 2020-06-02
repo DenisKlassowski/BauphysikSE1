e283f5ef262fe4e76ecd368edb6884faccec64d8
mupfel <wolframelp@gmail.com>
2020-06-02 16:10:09 +0200

    Additionen in Zusammenhang zum Dev-Meeting am 2.6. (Komfortfunktionen auf TabData, tab-namen+layermaterialien)

--- Prototype/main prototype/LayerData.py
+++ Prototype/main prototype/LayerData.py
@@ -1,11 +1,16 @@
 # This Python file uses the following encoding: utf-8
+import Calculation
 
 
 class LayerData:
-    def __init__(self, r, lambda_, width):
+    def __init__(self, mat, r, lambda_, width):
         self.width = width
         self.lambda_ = lambda_
         self.r = r
         self.t_inside = 0.0
         self.t_outside = 0.0
         self.rho = 0.0
+        self.mat = mat
+
+    def calculate(self):
+        Calculation.layer_calc(self)
--- Prototype/main prototype/TabData.py
+++ Prototype/main prototype/TabData.py
@@ -1,5 +1,8 @@
+import Calculation
+
 class TabData:
-    def __init__(self, mode, layers, rsi=0.0, rse=0.0, rges=0.0, rt=0.0, tin=0.0, tout=0.0, u=0.0):
+    def __init__(self, mode, name, layers, rsi=0.0, rse=0.0, rges=0.0, rt=0.0, tin=0.0, tout=0.0, u=0.0):
+        self.name = name
         self.layers = layers
         self.rsi = rsi
         self.rse = rse
@@ -9,3 +12,15 @@ class TabData:
         self.mode = mode
         self.u = u
         self.rt = rt
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+    def pop_layer(self, index):
+        self.layers.pop(index)
+
+    def insert_layer(self, layer, index):
+        self.layers.insert(index, layer)
+
+    def calculate(self):
+        Calculation.tab_calc(self)
